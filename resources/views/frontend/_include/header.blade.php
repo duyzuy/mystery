3921487d7df9556065a82fffe4f6f3ce99f2211e
Duy Vu <vutruongduy2109@gmail.com>
2020-08-17 12:57:20 +0700

    Update Dynamic language and fix css

--- resources/views/frontend/_include/header.blade.php
+++ resources/views/frontend/_include/header.blade.php
@@ -7,6 +7,8 @@
       <div class="navbar-item has-dropdown is-hoverable nav-lang-mobile">
         
         <a class="navbar-link" data-toggle="dropdown">
+
+
           @if(app()->getLocale() == 'en')
             <span class="navbar-lang">
               <i class="flag-icon flag-icon-us"></i> EN
@@ -18,13 +20,56 @@
           @endif
         </a>
         <div class="navbar-dropdown is-boxed">
-          <a href="{{ route('home', 'vi') }}" class="navbar-item dropdown-item">
-            <i class="flag-icon flag-icon-vn mr-2"></i> Tiếng việt
-          </a>
-          <a href="{{ route('home', 'en') }}" class="navbar-item dropdown-item">
-            <i class="flag-icon flag-icon-us mr-2"></i> English
-          </a>       
+          @if( Route::currentRouteName() == 'user.survey.detail')
+
+            @php 
+    
+              $slug = Request::segment(3);
+    
+    
+              $id = preg_match_all('!\d+!', $slug, $matches);
+              $id = implode(' ', $matches[0]);
+    
+              $slug = preg_replace('!\d+!', '', $slug);
+              $slug = substr($slug, 1);
+    
+            @endphp
+          
+
+              <a href="{{ route(Route::currentRouteName(), ['vi', $id, $slug]) }}" class="navbar-item dropdown-item">
+                <i class="flag-icon flag-icon-vn mr-2"></i> Tiếng việt
+              </a>
+              <a href="{{ route(Route::currentRouteName(), ['en', $id, $slug]) }}" class="navbar-item dropdown-item">
+                <i class="flag-icon flag-icon-us mr-2"></i> English
+              </a>       
+          @elseif(Route::currentRouteName() == 'user.thankyou')
+            @php 
+      
+            $slug = Request::segment(2);
+            $token = substr($slug, strrpos($slug, '=' )+1);
+
+          @endphp
+            <a href="{{ route(Route::currentRouteName(), ['vi', $token]) }}" class="navbar-item dropdown-item">
+              <i class="flag-icon flag-icon-vn mr-2"></i> Tiếng việt
+            </a>
+            <a href="{{ route(Route::currentRouteName(), ['en', $token]) }}" class="navbar-item dropdown-item">
+              <i class="flag-icon flag-icon-us mr-2"></i> English
+            </a>       
+
+          @else
+
+            <a href="{{ route(Route::currentRouteName(), 'vi') }}" class="navbar-item dropdown-item">
+              <i class="flag-icon flag-icon-vn mr-2"></i> Tiếng việt
+            </a>
+            <a href="{{ route(Route::currentRouteName(), 'en') }}" class="navbar-item dropdown-item">
+              <i class="flag-icon flag-icon-us mr-2"></i> English
+            </a>       
+
+          @endif
+   
         </div>
+
+
       </div>
 
       <div class="navbar-burger burger" data-target="mobileMenu">
@@ -143,8 +188,8 @@
       @endauth
       <div class="navbar-item has-dropdown is-hoverable">
         
-   
-
+      
+    
        
         <a class="navbar-link" data-toggle="dropdown">
           @if(app()->getLocale() == 'en')
@@ -158,7 +203,7 @@
           @endif
         </a>
         <div class="navbar-dropdown is-boxed">
-
+         
           @if( Route::currentRouteName() == 'user.survey.detail')
 
             @php 
@@ -175,10 +220,23 @@
             @endphp
           
 
-            <a href="{{ route(Route::currentRouteName(), ['vi', $id, $slug]) }}" class="navbar-item dropdown-item">
+              <a href="{{ route(Route::currentRouteName(), ['vi', $id, $slug]) }}" class="navbar-item dropdown-item">
+                <i class="flag-icon flag-icon-vn mr-2"></i> Tiếng việt
+              </a>
+              <a href="{{ route(Route::currentRouteName(), ['en', $id, $slug]) }}" class="navbar-item dropdown-item">
+                <i class="flag-icon flag-icon-us mr-2"></i> English
+              </a>       
+          @elseif(Route::currentRouteName() == 'user.thankyou')
+            @php 
+      
+            $slug = Request::segment(2);
+            $token = substr($slug, strrpos($slug, '=' )+1);
+
+          @endphp
+            <a href="{{ route(Route::currentRouteName(), ['vi', $token]) }}" class="navbar-item dropdown-item">
               <i class="flag-icon flag-icon-vn mr-2"></i> Tiếng việt
             </a>
-            <a href="{{ route(Route::currentRouteName(), ['en', $id, $slug]) }}" class="navbar-item dropdown-item">
+            <a href="{{ route(Route::currentRouteName(), ['en', $token]) }}" class="navbar-item dropdown-item">
               <i class="flag-icon flag-icon-us mr-2"></i> English
             </a>       
 
@@ -191,13 +249,12 @@
               <i class="flag-icon flag-icon-us mr-2"></i> English
             </a>       
 
-
           @endif
 
 
 
 
-        </div>
+        </div> 
       </div>
 
        
